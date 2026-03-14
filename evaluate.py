Fix gradient signs in backward pass

evaluate.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     tokens = preprocess(text)
     word2idx, idx2word = build_vocab(tokens)
-    embeddings = np.load("data/embeddings.npy")
+    embeddings = np.load("data/embeddings_epoch2.npy")
 
     test_words = ["king", "love", "sword", "death", "good"]
     for word in test_words:

train.py
@@ -20,17 +20,13 @@ def train(model, pairs, vocab_size, epochs=5, lr=0.0005):
         offset = epoch * len(pairs)
         
         for i, (center_idx, context_idx) in enumerate(pairs):
-            # Learning rate decay global
-            current_lr = lr * (1 - (offset + i) / n)
-            current_lr = max(current_lr, lr * 0.0001)
-            
             negative_indices = get_negative_samples(vocab_size, context_idx)
             
             loss, v_center, u_context, u_negatives, score_pos, scores_neg = model.forward(
                 center_idx, context_idx, negative_indices)
             
             model.backward(center_idx, context_idx, negative_indices,
-                         v_center, u_context, u_negatives, score_pos, scores_neg, current_lr)
+                        v_center, u_context, u_negatives, score_pos, scores_neg, lr)
             
             total_loss += loss
             
@@ -55,7 +51,7 @@ if __name__ == "__main__":
     vocab_size = len(word2idx)
     model = Word2Vec(vocab_size, embedding_dim=100)
 
-    train(model, pairs, vocab_size, epochs=5, lr=0.0005)
+    train(model, pairs, vocab_size, epochs=20, lr=0.001)
 
     np.save("data/embeddings.npy", model.W_center)
     print("Embeddings saved!")

word2vec.py
@@ -24,14 +24,14 @@ class Word2Vec:
         v_center, u_context, u_negatives, score_pos, scores_neg, lr=0.01):
     
         # Gradients
-        grad_v = (score_pos - 1) * u_context - np.sum((1 - scores_neg)[:, None] * u_negatives, axis=0)
-        grad_u_pos = (score_pos - 1) * v_center
-        grad_u_neg = -(1 - scores_neg)[:, None] * v_center
+        grad_v = (1 - score_pos) * u_context + np.sum((1 - scores_neg)[:, None] * u_negatives, axis=0)
+        grad_u_pos = (1 - score_pos) * v_center
+        grad_u_neg = (1 - scores_neg)[:, None] * v_center
 
         # Gradient clipping
-        grad_v = np.clip(grad_v, -2, 2)
-        grad_u_pos = np.clip(grad_u_pos, -2, 2)
-        grad_u_neg = np.clip(grad_u_neg, -2, 2)
+        grad_v = np.clip(grad_v, -1, 1)
+        grad_u_pos = np.clip(grad_u_pos, -1, 1)
+        grad_u_neg = np.clip(grad_u_neg, -1, 1)
 
         # Updates avec weight decay
         self.W_center[center_idx] -= lr * grad_v + 1e-5 * self.W_center[center_idx]